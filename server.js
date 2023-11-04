11b6ab8faac1beeba0218e978a570757ee68a0e5
xluo8 <xingyanluo@gmail.com>
2023-11-04 15:18:05 -0500

    Added inventory product loading functionality

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
 // Load the necessary modules
 var express = require('express');
 var app = express();
+var fetch = require('node-fetch');
 
 // Set the view engine to EJS and specify the views directory
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); // Ensure views directory path is set correctly
 
-// Serve static files (e.g., CSS, JavaScript)
+// Serve static files (if you have any)
 app.use(express.static(__dirname + '/public'));
 
 // Routes
 
 // Index page
-app.get('/', function(req, res) {
-    // You can use the 'fetch' method to get data from the API
-    fetch('https://dummyjson.com/products')
-        .then(response => response.json())
-        .then(data => {
-            // Process the data to select the relevant attributes
-            const products = data.products.map(product => ({
-                title: product.title,
-                price: product.price,
-                discountPercentage: product.discountPercentage
-            }));
-
-            res.render('index', {
-                products: products
-            });
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('Internal Server Error');
-        });
+app.get('/', function (req, res) {
+    res.render('index', { products: null }); // Initially, pass null to products
 });
 
 // Listen on port 8081
