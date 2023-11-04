1e572a1c871803c4b8ad743322853285079e6933
xluo8 <xingyanluo@gmail.com>
2023-11-04 16:03:25 -0500

    update server.js to fetch data

--- server.js
+++ server.js
@@ -17,6 +17,27 @@ app.get('/', function (req, res) {
     res.render('index', { products: null }); // Initially, pass null to products
 });
 
+// Add a new route for fetching products from the API
+app.get('/getProducts', function (req, res) {
+    fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        .then(data => {
+            const products = data.products.map(product => ({
+                title: product.title,
+                price: product.price,
+                discountPercentage: product.discountPercentage,
+                finalPrice: (product.price - (product.price * product.discountPercentage / 100)).toFixed(2),
+                thumbnail: product.thumbnail
+            }));
+
+            res.json({ products }); // Send JSON response with products
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
 // Listen on port 8081
 app.listen(8081, function () {
     console.log('8081 is the magic port');
